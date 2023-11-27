c67ccb9725ebc2d0ab4a7dbe07d2b70e82029b92
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-11-27 09:57:10 +0500

    add uchburchak

--- src/Uchburchak.java
+++ src/Uchburchak.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Uchburchak {
+public class Uchburchak {
+    private int a;
+    private int b;
+    private int c;
+
+    public Uchburchak(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+
+    }
 }
